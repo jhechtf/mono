014aa8bc7673ab40ee022865baa808dd6a5b6c7b
Jim Burbridge <jhechtf@gmail.com>
2024-02-10 00:51:49 -0800

    wip: add in types from trashs server

--- packages/design-tokens/src/types.ts
+++ packages/design-tokens/src/types.ts
@@ -8,12 +8,6 @@ export interface TypeMap {
   [type: string]: TokenMap;
 }
 
-export type Config = TypeMap & {
-  variants?: {
-    [query: string]: TypeMap;
-  };
-};
-
 export type CConfig = {
   variants: {
     [query: string]: {
@@ -75,3 +69,72 @@ export const config = {
  *    }
  * }
  */
+
+const makeConfig = <T extends Record<string, unknown>>(
+  config: WithReferences<T>,
+) =>
+  // TODO: resolve references at runtime here
+  config as ResolveReferences<T>;
+
+export type Config = typeof config;
+
+type WithReferences<T extends Record<string, unknown>> =
+  ObjectKeyPaths<T> extends infer Keys extends string
+    ? WithReferencesHelper<T, `!${Keys}`>
+    : never;
+
+type WithReferencesHelper<T, Keys extends string> = T extends object
+  ? {
+      [Key in keyof T]: T[Key] extends string
+        ? // TODO: disallow circular references
+          Keys | (string & {})
+        : WithReferencesHelper<T[Key], Keys>;
+    }
+  : T;
+
+type ResolveReferences<T, Root = T> = T extends object
+  ? {
+      [Key in keyof T]: T[Key] extends `!${infer Path extends string}`
+        ? Get<Root, Path>
+        : ResolveReferences<T[Key], Root>;
+    }
+  : T;
+
+type ObjectKeyPaths<
+  T extends object,
+  IsRoot extends boolean = true,
+  K extends keyof T = keyof T,
+> = K extends string | number
+  ?
+      | ExpandKey<K, IsRoot>
+      | (T[K] extends object
+          ? `${ExpandKey<K, IsRoot>}${ObjectKeyPaths<T[K], false>}`
+          : never)
+  : never;
+
+type ExpandKey<
+  K extends string | number,
+  IsRoot extends boolean,
+> = IsRoot extends true ? `${K}` : `.${K}`;
+
+type Get<T, K> = K extends `${infer Key}.${infer Rest}`
+  ? Get<GetKey<T, Key>, Rest>
+  : GetKey<T, K>;
+
+type GetKey<T, K> = K extends keyof T ? T[K] : never;
+
+const conf = makeConfig({
+  a: 'string',
+  b: '!a',
+  c: {
+    nested: 1,
+    b: 3,
+    v: {
+      deeply: {
+        nested: 'bob',
+      },
+    },
+  },
+  d: '!c.nested',
+  e: '!c.v.deeply.nested',
+});
